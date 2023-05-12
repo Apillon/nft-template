d8847fbe53658393e1fdd275ed0d38c7a53e06b5
CryptoBWI <63158711+CryptoBWI@users.noreply.github.com>
2023-05-12 19:49:35 +0200

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const nftAddress = "1b226f3d-b2eb-4a62-a1a6-f0f41afa447a"; // Paste the address of the NFT collection.
+const nftAddress = ""; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
 const chainId = "0x507"; // Moonbase
 // const chainId = "0x504"; // Moonbeam
