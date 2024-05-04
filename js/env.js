90d54998f662683f79647ac902330f90c7d669d6
Abdulyb <168655603+Abdulyb@users.noreply.github.com>
2024-05-04 22:39:48 +0100

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,5 +1,5 @@
-const contractAddress = ""; // Paste the address of the NFT collection.
-// uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
-// const chainId = "0x504"; // Moonbeam
+const contractAddress = "0xFf5952519981D5e597B0F456760aEadF43e8891F"; // Your Moonbeam contract address
+// uncomment the chain you  are using by deleting the slashes
+// const chainId = "0x507"; // Moonbase
+const chainId = "0x504"; // Moonbeam
 // const chainId = "0x250"; // Astar
