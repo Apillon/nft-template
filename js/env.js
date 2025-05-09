15d941aca695b60061a55d294fe4d0886e2cb500
Tadej <tadej.dev@gmail.com>
2025-05-09 11:33:59 +0200

    fet(chains): add support for EVM chains
    - add a few EVM chains
    - refactor addChain to simplify and reduce duplicated code

--- js/env.js
+++ js/env.js
@@ -1,5 +1,29 @@
-const contractAddress = ""; // Paste the address of the NFT collection.
-// uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
-// const chainId = "0x504"; // Moonbeam
-// const chainId = "0x250"; // Astar
+const contractAddress = "0xc5177F25506A651592a134E33012e162ABa2Be59"; // Paste the address of the NFT collection.
+
+// Production networks
+const ETHEREUM = 1;
+const MOONBEAM = 1284;
+const ASTAR = 592;
+const OASIS_SAPPHIRE = 23294;
+const CELO = 42220;
+const BASE = 8453;
+const ARBITRUM_ONE = 42161;
+const AVALANCHE = 43114;
+const OPTIMISM = 10;
+const POLYGON = 137;
+
+// Test networks
+const SEPOLIA = 11155111;
+const MOONBASE = 1287;
+const ASTAR_SHIBUYA = 81;
+const OASIS_TESTNET = 42262;
+const ALFAJORES = 44787;
+const BASE_SEPOLIA = 84788;
+const ARBITRUM_ONE_SEPOLIA = 421614;
+const AVALANCHE_FUJI = 43113;
+const OPTIMISM_SEPOLIA = 11155420;
+const POLYGON_AMOY = 80002;
+
+// Set active chain
+const chainId = MOONBASE; // Change this to switch networks
+
